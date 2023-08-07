c648a771ce05cecefb975b7160782edebcc90aab
cherrysctse <133928214+cherrysctse@users.noreply.github.com>
2023-08-07 11:49:48 -0700

    update bkend code

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,12 +9,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-//const uri = "mongodb://127.0.0.1:27017/activitiescollections";
-//const uri = "mongodb+srv://cherry703:q4073rqyOSNSVnGO@cluster0.ac1mmob.mongodb.net/activitiescollections";
-const uri = "mongodb+srv://cherry703:12345@cluster0.ahtiyhi.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
-);
+const uri = process.env.ATLAS_URL;
+
+mongoose.connect(uri);
+
+mongoose.set()
+
 
 const connection = mongoose.connection;
 connection.once('open', () => {
